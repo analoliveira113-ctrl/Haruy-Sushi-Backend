0324c7710b7288cfcf375cd2e8b8fab60f587324
analoliveira113-ctrl <ana.l.oliveira113@aluno.senai.br>
2026-02-24 10:28:47 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ app.use(express.json());
 let produtos = [
     {id: 1, nome: "Temaki Completo", preco: 35.50, categoria: "temakis"},
     {id: 2, nome: "Combinado 20 Peças", preco: 85.00, categoria: "sushi"},
+    {id: 3, nome: "Temaki Filadélfia", preco: 28.00, categoria: "temakis"},
+    {id: 4, nome: "Temaki Salmão", preco: 85.00, categoria: "temakis"},
+    {id: 5, nome: "Temaki Camarão", preco: 32.00, categoria: "temakis"},
+    {id: 6, nome: "Temaki Hot", preco: 30.00, categoria: "temakis"},
+    {id: 7, nome: "Temaki Atum", preco: 85.00, categoria: "temakis"},
+    {id: 8, nome: "Combinado Sushi Premium", preco: 115.00, categoria: "sushi"},
+    {id: 9, nome: "Combinado Hot", preco: 99.00, categoria: "sushi"},
+    {id: 10, nome: "Combinado Hot Premium", preco: 120.00, categoria: "sushi"},
+    {id: 11, nome: "Niguiri Variado (10un)", preco: 45.00, categoria: "sushi"},
+    {id: 12, nome: "Niguiri Variado (12un)", preco: 55.00, categoria: "sushi"},
+    {id: 13, nome: "Refrigerante", preco: 8.00, categoria: "bebidas"},
+    {id: 14, nome: "Água Mineral", preco: 4.00, categoria: "bebidas"},
+    {id: 15, nome: "Cerveja Heineken", preco: 14.00, categoria: "bebidas"},
+    {id: 16, nome: "Suco Natural", preco: 12.00, categoria: "bebidas"},
+    {id: 17, nome: "Sakê quente", preco: 18.00, categoria: "bebidas"},
+    {id: 18, nome: "Chá verde", preco: 7.00, categoria: "bebidas"},
+    {id: 19, nome: "Mochi de Morango", preco: 12.00, categoria: "sobremesas"},
+    {id: 20, nome: "Mochi de Matchá", preco: 12.00, categoria: "sobremesas"},
+    {id: 21, nome: "Dorayaki", preco: 10.00, categoria: "sobremesas"},
+    {id: 22, nome: "Sorvete de Matchá", preco: 12.00, categoria: "sobremesas"},
+    {id: 23, nome: "Tempura de Banana", preco: 14.00, categoria: "sobremesas"},
+
 ];
 //listar produtos
 app.get('/produtos', (req, res) => {
